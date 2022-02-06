c18eaff5ae4f0587b23a3387d9b20135e7edfdc0
Harsh Modi <harshmodi@Harshs-MacBook-Pro.local>
2022-02-06 10:49:58 -0800

    Menu Design Updated

--- Aether/Assets/Scripts/GroundSpawner.cs
+++ Aether/Assets/Scripts/GroundSpawner.cs
@@ -11,23 +11,19 @@ public class GroundSpawner : MonoBehaviour {
     [SerializeField] GameObject groundTileFinish;
     Vector3 nextSpawnPoint;
 
-
-    int[] arr = { 0, 0, 0, 0, 10, 10, 10, 10, 10, 0, 0, 0, 0, -10, -10, -10, -20, -20, 0, 0, 40 };
-    List<(int Index, string Name)> pathCoordinates = new List<(int Index, string Name)>
+    List<(int X_Value, int Z_Value, string Name)> pathCoordinates = new List<(int X_Value, int Z_Value, string Name)>
       {
-          (0, "Blue"),(0, "Blue"),(0, "Blue"),(0, "Blue"),(0, "Finish"),
-          (10, "Red"),(10, "Red"),(10, "Red"),
-          (-10,"Green"),(-10,"Green"),(-10,"Green"),(-10,"Green"),
-          (0, "Blue"),(0, "Blue"),(0, "Blue"),(0, "Blue"),(0,"Blue"),
-          (10,"Yellow"),(10,"Yellow"),
-          (-10, "Red"),(-10, "Red"),(-10, "Red"),
-          (0, "Green"),(0, "Green"),(0, "Finish")
+          (0,0,"Blue"),(0,10,"Blue"),(0,20,"Blue"),(0,30,"Blue"),(0,40,"Blue"),(0,50,"Blue"),(0,60,"Blue"),(10,60,"Red"),(0,70,"Blue"),(10,70, "Red"),
+          (10,80, "Red"),(10,90, "Red"),
+          (-10,80,"Green"),(-10,90,"Green"),(-10,100,"Green"),(-10,110,"Green"),(-10,120,"Green"),(-10,130,"Green"),(-10,140,"Green"),(-10,150,"Green"),
+          (0,170, "Red"),(0,180, "Red"),(0,190, "Red"),(0,200, "Green"),
+          (0,220,"Blue"),(10,220,"Yellow"),(0,230,"Blue"),(10,230,"Yellow"),(0,240,"Blue"),(10,240,"Yellow"),(0,250,"Blue"),(0,260,"Blue"),(0,270,"Blue"),
+          (0,290,"Green"),(0,300,"Green"),(0,310,"Green"),(0,320,"Finish"),
       };
     int i = 0;
 
 
     public void SpawnTile(bool spawnItems) {
-        //Debug.Log("Index - " + pathCoordinates[i].Index + " name - " + pathCoordinates[i].Name);
         int curr = (int)nextSpawnPoint.x;
 
         GameObject tempGroundTileObject = null;
@@ -67,7 +63,8 @@ public class GroundSpawner : MonoBehaviour {
         }
 
         nextSpawnPoint = tempGroundTileObject.transform.GetChild(1).transform.position;
-        nextSpawnPoint.x = pathCoordinates[i + 1].Index;
+        nextSpawnPoint.x = pathCoordinates[i + 1].X_Value;
+        nextSpawnPoint.z = pathCoordinates[i + 1].Z_Value;
         i++;
 
         if (spawnItems) {
