ba76407ceca294907e7edabe93465b926c17d4a7
Harsh Modi <harshmodi@Harshs-MacBook-Pro.local>
2022-02-06 16:57:32 -0800

    Background Audio Muted

--- Aether/Assets/Scripts/AudioManager.cs
+++ Aether/Assets/Scripts/AudioManager.cs
@@ -33,7 +33,7 @@ public class AudioManager : MonoBehaviour {
     }
 
     void Start() {
-        Play("SpaceTravel");
+        //Play("SpaceTravel");
     }
 
     public void StopPlaying(string sound) {
