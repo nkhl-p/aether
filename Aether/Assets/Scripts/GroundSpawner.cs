176ab7f1ec8c7fe5681dbfe8482ee700b9603ba5
Ruchit Bhardwaj <ruchitbh@usc.edu>
2022-02-10 16:49:40 -0800

    Power-Up spawning posiiton can now be controlled manually

--- Aether/Assets/Scripts/GroundSpawner.cs
+++ Aether/Assets/Scripts/GroundSpawner.cs
@@ -11,14 +11,45 @@ public class GroundSpawner : MonoBehaviour {
     [SerializeField] GameObject groundTileFinish;
     Vector3 nextSpawnPoint;
 
-    List<(int X_Value, int Z_Value, string Name)> pathCoordinates = new List<(int X_Value, int Z_Value, string Name)>
+    List<(int X_Value, int Z_Value, string Name, bool isPowerUpEnabled)> pathCoordinates = new List<(int X_Value, int Z_Value, string Name, bool isPowerUpEnabled)>
       {
-          (0,0,"Blue"),(0,10,"Blue"),(0,20,"Blue"),(0,30,"Blue"),(0,40,"Blue"),(0,50,"Blue"),(0,60,"Blue"),(10,60,"Red"),(0,70,"Blue"),(10,70, "Red"),
-          (10,80, "Red"),(10,90, "Red"),
-          (-10,80,"Green"),(-10,90,"Green"),(-10,100,"Green"),(-10,110,"Green"),(-10,120,"Green"),(-10,130,"Green"),(-10,140,"Green"),(-10,150,"Green"),
-          (0,170, "Red"),(0,180, "Red"),(0,190, "Red"),(0,200, "Green"),
-          (0,220,"Blue"),(10,220,"Yellow"),(0,230,"Blue"),(10,230,"Yellow"),(0,240,"Blue"),(10,240,"Yellow"),(0,250,"Blue"),(0,260,"Blue"),(0,270,"Blue"),
-          (0,290,"Green"),(0,300,"Green"),(0,310,"Green"),(0,320,"Finish"),
+          (0,0,"Blue",false),
+(0,10,"Blue",false),
+(0,20,"Blue",false),
+(0,30,"Blue",false),
+(0,40,"Blue",false),
+(0,50,"Blue",false),
+(0,60,"Blue",false),
+(10,60,"Red",false),
+(0,70,"Blue",false),
+(10,70, "Red",false),
+(10,80, "Red",false),
+(10,90,"Red",false),
+(-10,80,"Green",false),
+(-10,90,"Green",false),
+(-10,100,"Green",true),
+(-10,110,"Green",false),
+(-10,120,"Green",false),
+(-10,130,"Green",false),
+(-10,140,"Green",false),
+(-10,150,"Green",false),
+(0,170, "Red",false),
+(0,180, "Red",false),
+(0,190, "Red",false),
+(0,200, "Green",false),
+(0,220,"Blue",false),
+(10,220,"Yellow",false),
+(0,230,"Blue",false),
+(10,230,"Yellow",false),
+(0,240,"Blue",false),
+(10,240,"Yellow",false),
+(0,250,"Blue",false),
+(0,260,"Blue",false),
+(0,270,"Blue",false),
+(0,290,"Green",false),
+(0,300,"Green",false),
+(0,310,"Green",false),
+(0,320,"Finish",false),
       };
     int i = 0;
 
@@ -32,6 +63,7 @@ public class GroundSpawner : MonoBehaviour {
 
             case 0:
                 var color1 = pathCoordinates[i].Name;
+                bool isPowerUpEnabled1 = pathCoordinates[i].isPowerUpEnabled;
                 switch (color1) {
                     case "Red": tempGroundTileObject = Instantiate(groundTileRed, nextSpawnPoint, Quaternion.identity); break;
                     case "Blue": tempGroundTileObject = Instantiate(groundTileBlue, nextSpawnPoint, Quaternion.identity); break;
@@ -39,9 +71,13 @@ public class GroundSpawner : MonoBehaviour {
                     case "Yellow": tempGroundTileObject = Instantiate(groundTileYellow, nextSpawnPoint, Quaternion.identity); break;
                     case "Finish": tempGroundTileObject = Instantiate(groundTileFinish, nextSpawnPoint, Quaternion.identity); break;
                 }
+                if (isPowerUpEnabled1) {
+                    tempGroundTileObject.GetComponent<GroundTile>().SpawnPowerups();
+                }
                 break;
             case 10:
                 var color2 = pathCoordinates[i].Name;
+                bool isPowerUpEnabled2 = pathCoordinates[i].isPowerUpEnabled;
                 switch (color2) {
                     case "Red": tempGroundTileObject = Instantiate(groundTileRed, nextSpawnPoint, Quaternion.identity); break;
                     case "Blue": tempGroundTileObject = Instantiate(groundTileBlue, nextSpawnPoint, Quaternion.identity); break;
@@ -49,15 +85,22 @@ public class GroundSpawner : MonoBehaviour {
                     case "Yellow": tempGroundTileObject = Instantiate(groundTileYellow, nextSpawnPoint, Quaternion.identity); break;
                     case "Finish": tempGroundTileObject = Instantiate(groundTileFinish, nextSpawnPoint, Quaternion.identity); break;
                 }
+                if (isPowerUpEnabled2) {
+                    tempGroundTileObject.GetComponent<GroundTile>().SpawnPowerups();
+                }
                 break;
             case -10:
                 var color3 = pathCoordinates[i].Name;
+                bool isPowerUpEnabled3 = pathCoordinates[i].isPowerUpEnabled;
                 switch (color3) {
                     case "Red": tempGroundTileObject = Instantiate(groundTileRed, nextSpawnPoint, Quaternion.identity); break;
                     case "Blue": tempGroundTileObject = Instantiate(groundTileBlue, nextSpawnPoint, Quaternion.identity); break;
                     case "Green": tempGroundTileObject = Instantiate(groundTileGreen, nextSpawnPoint, Quaternion.identity); break;
                     case "Yellow": tempGroundTileObject = Instantiate(groundTileYellow, nextSpawnPoint, Quaternion.identity); break;
                     case "Finish": tempGroundTileObject = Instantiate(groundTileFinish, nextSpawnPoint, Quaternion.identity); break;
+                }
+                if (isPowerUpEnabled3) {
+                    tempGroundTileObject.GetComponent<GroundTile>().SpawnPowerups();
                 }
                 break;
         }
@@ -67,9 +110,9 @@ public class GroundSpawner : MonoBehaviour {
         nextSpawnPoint.z = pathCoordinates[i + 1].Z_Value;
         i++;
 
-        if (spawnItems) {
+        if (spawnItems && pathCoordinates[i].Z_Value != 100) {
             tempGroundTileObject.GetComponent<GroundTile>().SpawnObstacles();
-            tempGroundTileObject.GetComponent<GroundTile>().SpawnPowerups();
+            //tempGroundTileObject.GetComponent<GroundTile>().SpawnPowerups();
         }
     }
 
