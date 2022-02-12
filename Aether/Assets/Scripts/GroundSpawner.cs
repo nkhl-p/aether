000e2c391425a21e8381b6763547fa38c59f85ad
Ruchit Bhardwaj <ruchitbh@usc.edu>
2022-02-11 19:02:54 -0800

    Added logging

--- Aether/Assets/Scripts/GroundSpawner.cs
+++ Aether/Assets/Scripts/GroundSpawner.cs
@@ -118,8 +118,14 @@ public class GroundSpawner : MonoBehaviour {
 
         float randomObstacleChance = Random.Range(0f, 1f);
 
-        if (spawnItems && randomObstacleChance > obstacleSpawningChance) {
+        if (spawnItems && !pathCoordinates[i].IsPowerUpEnabled && randomObstacleChance > obstacleSpawningChance) {
             tempGroundTileObject.GetComponent<GroundTile>().SpawnObstacles();
+        } else {
+            Debug.Log(i);
+            Debug.Log("Random obstacle not spawned on tile with details " +
+                pathCoordinates[i].X_Value + " " + pathCoordinates[i].Z_Value + " " +
+                pathCoordinates[i].Name + " " + pathCoordinates[i].IsPowerUpEnabled + " " +
+                " as random number is " + randomObstacleChance);
         }
     }
 
