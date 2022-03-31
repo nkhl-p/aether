d0d24e25f0cd8717fa1136979d597a4509081cf4
Nikhil Patel <nikhil.mp96@gmail.com>
2022-03-30 17:49:17 -0700

    add speedup powerup

--- Aether/Assets/Scripts/PlayerMovement.cs
+++ Aether/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ using System;
 
 public class PlayerMovement : MonoBehaviour {
     public float speed = 5;
+    public int powerUpSpeed = 0;
     [SerializeField] Rigidbody rb;
 
     bool alive = true;
@@ -348,6 +349,6 @@ public class PlayerMovement : MonoBehaviour {
                 baseSpeed = 0;
                 break;
         }
-        return baseSpeed;
+        return baseSpeed + powerUpSpeed;
     }
 }
