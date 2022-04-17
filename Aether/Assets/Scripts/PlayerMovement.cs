299ecca76069d014b63b3321d509116c1d3b0fa2
Nikhil Patel <nikhil.mp96@gmail.com>
2022-04-16 20:12:55 -0700

    Revert "Feature/add popup template"

--- Aether/Assets/Scripts/PlayerMovement.cs
+++ Aether/Assets/Scripts/PlayerMovement.cs
@@ -37,15 +37,11 @@ public class PlayerMovement : MonoBehaviour {
     public static int powerUpsLevelCount = 0;
     bool val = false;
 
-    private bool freezeGame = false;
     // variable declaration for player health
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
 
-  
-  
-  
     private void Awake() {
         transformCache = transform;
     }
@@ -59,11 +55,6 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        if (freezeGame)
-        {
-            return;
-        }
-        
         if (!alive) return;
         float curr_speed = speed;
         if (powerUpSpeed > 0) {
@@ -76,10 +67,6 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void Update() {
-        if (freezeGame)
-        {
-            return;
-        }
         horizontalInput = Input.GetAxis("Horizontal");
 
         if (Input.GetKeyDown(KeyCode.Space)) {
@@ -160,21 +147,6 @@ public class PlayerMovement : MonoBehaviour {
         // Depending on what is decided (restart same scene or show pause/quit menu, the following line of code will change
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         audioManagerInstance.Play(SoundEnums.THEME.GetString());
-    }
-
-    public void FreezeGame()
-    {
-        freezeGame = true;
-    }
-
-    public void UnFreezeGame()
-    {
-        freezeGame = false;
-    }
-
-    public bool getGameFreeze()
-    {
-        return freezeGame;
     }
 
     void Jump() {
